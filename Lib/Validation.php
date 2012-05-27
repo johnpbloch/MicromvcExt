a0871f12f52516919081d57c8a9e9477508fb33a
John P. Bloch <johnpbloch@gmail.com>
2012-05-26 20:43:11 -0400

    Add a few more validation options:
        * float_rule: test whether the value is a float
        * numeric_rule: test whether the value is numeric (float, int, or numeric string)
        * greater_than_rule: test whether the value is greater than another value
        * less_than_rule: test whether the value is less than another value

--- Lib/Validation.php
+++ Lib/Validation.php
@@ -4,15 +4,16 @@ namespace MicromvcExt\Lib;
 
 class Validation extends \Core\Validation
 {
+
 	/**
 	 * Check to see if the email entered is valid.
 	 *
 	 * @param string $data to validate
 	 * @return boolean
 	 */
-	protected function email_rule($data)
+	protected function email_rule( $data )
 	{
-		return (bool) filter_var($data, FILTER_VALIDATE_EMAIL);
+		return (bool)filter_var( $data, FILTER_VALIDATE_EMAIL );
 	}
 
 	/**
@@ -22,9 +23,54 @@ class Validation extends \Core\Validation
 	 * @param string $regex the regex to check the data against
 	 * @return boolean
 	 */
-	protected function pattern_rule($data, $pattern)
+	protected function pattern_rule( $data, $pattern )
 	{
-		return preg_match($pattern, $data);
+		return preg_match( $pattern, $data );
 	}
+
+	/**
+	 * Test a value to see if it is a float
+	 * @param mixed $data to validate
+	 * @return boolean
+	 */
+	protected function float_rule( $data )
+	{
+		return is_float( $data );
+	}
+
+	/**
+	 * Test whether a value is numeric
+	 * @param mixed $data
+	 * @return bool
+	 */
+	protected function numeric_rule( $data )
+	{
+		return is_numeric( $data );
+	}
+
+	/**
+	 * Test whether a value is greater than another value
+	 * 
+	 * @param int|float $data
+	 * @param int|float $value
+	 * @return type bool
+	 */
+	protected function greater_than_rule( $data, $value )
+	{
+		return $data > $value;
+	}
+
+	/**
+	 * Test whether a value is less than another value
+	 * 
+	 * @param int|float $data
+	 * @param int|float $value
+	 * @return bool 
+	 */
+	protected function less_than_rule( $data, $value )
+	{
+		return $data < $value;
+	}
+
 }
 
